8d5507779a63fc528e72999ee0ce6a89537e154b
Deepesh Kumar Yogi <57905352+Deepesh8122@users.noreply.github.com>
2023-11-06 06:19:17 +0530

    Page done

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,17 +6,36 @@ module.exports = {
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
       },
+      colors: {
+        'gray-999': 'rgb(23 23 23)',
+        'gray-800': 'rgb(38 38 38)'
+      },
       animation: {
         'spin-slow': 'spin 3s linear infinite',
-        'reverse-spin' : 'reverse-spin 3s linear infinite'
+        'reverse-spin' : 'reverse-spin 3s linear infinite',
+        'marquee': 'marquee 30s linear infinite',
+        'marquee2': 'marquee 40s linear infinite',
+        'marquee3': 'marquee 35s linear infinite',
       },
       keyframes: {
         'reverse-spin': {
           from: {
             transform: 'rotate(360deg)'
           }
-        }
-      }
+        },
+        'marquee': {
+          from: { transform: 'translateY(0%)' },
+          to: { transform: 'translateY(-100%)' },
+        },
+        'marquee2': {
+          '0%': { transform: 'translateY(100%)' },
+          '100%': { transform: 'translateY(0%)' },
+        },
+        'marquee3': {
+          '0%': { transform: 'translateY(100%)' },
+          '100%': { transform: 'translateY(0%)' },
+        },
+      },
     },
   },
   plugins: [],
